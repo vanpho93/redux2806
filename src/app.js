09dfaf4fb21a693dbe17e81869ad6e6f5d380e07
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-10 15:03:42 +0700

    ...

--- src/app.js
+++ src/app.js
@@ -10,3 +10,12 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root') // eslint-disable-line
 );
+
+// const person = { name: 'Pho', age: 18 };
+// const p2 = { ...person, height: 20 };
+
+// const nums = [1, 3, 4, 2, 4];
+
+// const nums2 = [2000, ...nums, 1000];
+// console.log(nums);
+// console.log(nums2);

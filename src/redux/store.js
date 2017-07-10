61ca09407591e8d7c6dab014fc0cb312f3c50941
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-10 15:07:55 +0700

    reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,10 +17,13 @@ const defaultState = {
         { id: 13, en: 'against', vn: 'phản đối', memorized: true, isShow: false },
         { id: 14, en: 'arrange', vn: 'sắp xếp', memorized: false, isShow: false }
     ],
-    filterMode: 'SHOW_ALL'
+    filterMode: 'MEMORIZED'
 };
 
 const reducer = (state = defaultState, action) => {
+    if (action.type === 'FILTER_SHOW_ALL') return { ...state, filterMode: 'SHOW_ALL' };
+    if (action.type === 'FILTER_SHOW_MEMORIZED') return { ...state, filterMode: 'MEMORIZED' };
+    if (action.type === 'FILTER_NEED_PRACTICE') return { ...state, filterMode: 'NEED_PRACTICE' };
     return state;
 };
 

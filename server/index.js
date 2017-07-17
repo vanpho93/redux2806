0c529ddc959b693c0eb3705f53cba6074e79a868
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-17 15:43:44 +0700

    break

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+
+// use it before all route definitions
+
 const Word = require('./Word');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('./public'));
+
 app.listen(3000, () => console.log('Server is running'));
 
 app.get('/words', (req, res) => {
     Word.getAll()
-    .then(words => res.send(words));
+    .then(words => {
+        res.send(words);
+    });
 });
+
+app.get('/', (req, res) => res.render('home'));
